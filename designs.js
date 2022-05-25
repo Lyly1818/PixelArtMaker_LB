022ead18acfc9eb4e522abb31973f054eb386a43
Lesly Blaise <lpblaise@bhcc.edu>
2022-05-25 17:10:39 -0400

    fixed height and width issues reset table when new H and  W are submitted

--- designs.js
+++ designs.js
@@ -1,71 +1,60 @@
 // Select color input
 // Select size input
+//the driver launch the program and build initial table
+function driver(){
+    //access the form element
+    let sizeForm = document.getElementById("sizePicker");
+    let height = 10
+    let width = 10
+    makeGrid(height, width)//take initial H and W
+    submit_btn(sizeForm); //
+}
 
-let  canvas  = document.getElementById("pixelCanvas");
-let height = document.getElementById("inputHeight").value;
-let width = document.getElementById("inputWidth").value;
-let submitBtn =  document.querySelector("input[type ='submit']");
-// let color = $('#colorPicker').val();
+driver();
 
-
-
-// // Sets the color to default (i.e. Black)
-// let selectedColor = $('#colorPicker').val();
-// // Updates the color to the selected color by the user.
-// $('#colorPicker').on('input', function() {
-//     // Fetching the user's input
-//     selectedColor = $('#colorPicker').val();
-// });
-
-// color.addEventListener("input", changeTracker, false );
-// function changeTracker(event) {
-//     document.querySelectorAll("").forEach(function(p) {
-//         p.style.color = event.target.value;
-//         color = p.style.color;
-//
-//     });
-// }
-// function colorPicker(){
-//     let color = document.getElementById("colorPicker");
-//     color.addEventListener("input", event=>{
-//         color.style.color = event.target.value;
-//         console.log(color);
-//     })
-//      return color.value;
-// }
-
-submitBtn.addEventListener("click", ()=>{
-    // let width_a = $("inputWidth").val();
-    makeGrid(width, height);
-    // console.log("value: " + height);
-});
-
-
-
-function click_fill_cell( cell){
-    $("#colorPicker").on("input", function (){
-        color = $("#colorPicker").val();
-    });
-
-    cell.addEventListener("click", event=>{
-
+//take a row element and change color background
+//when click event is registered.
+function click_fill_cell(cell) {
+    cell.addEventListener("click", event => {
         cell.style.background = $('#colorPicker').val()
 
     });
 }
+//take column and row elements and build
+// a table by appending rows to table at each iteration
+//it returns a table at the end
 
-function makeGrid(num_row, num_column ) {
-    let table  =  document.getElementById("pixelCanvas");
-    for(let i = 0; i < num_row; i++){
+function makeGrid(height_val, width_val) {
+    let table = document.getElementById("pixelCanvas");
+    for (let i = 0; i < height_val; i++) {
         let rows = document.createElement("tr");
-        for(let j = 0; j < num_column; j++){
+        for (let j = 0; j < width_val; j++) {
             let columns = document.createElement("td");
             click_fill_cell(columns)
             rows.appendChild(columns);
         }
         table.appendChild(rows);
+
     }
     return table;
 }
+//Take a form, register a submit event
+//
+function submit_btn(form) {
+   // let initialTable =  makeGrid(height, width)
+   //  console.log("BEFORE THE EVENT")
+    form.addEventListener("submit", event => {
+        reset(document.querySelector("table"));//reset current table
+        let height = form.elements["height"].value;// access current  H value
+        let width = form.elements["width"].value; //access current  W value
+        makeGrid(height, width)// build a new table
+        // console.log("AFTER THE EVENT: "+"width: " + width + " height: " + height)
+        event.preventDefault() // prevent reloading at submit event
+    });
+}
 
-document.appendChild(makeGrid(4,4));
+function reset(table) {
+    table.innerText ="";
+}
+
+
